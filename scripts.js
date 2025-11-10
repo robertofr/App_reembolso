076aa3a9208396aa2b9d07a14530dbeb44ed570b
Roberto F. Rodrigues <robertofr_@hotmail.com>
2025-11-10 17:15:15 -0300

    Formatação dos valores

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log("Script loaded successfully.")
+//Seleciona elementos do form.
+const amount = document.getElementById("amount")
+
+
+// Captura o evento de input no campo de valor.
+amount.oninput = () =>{
+// Obtém o valor atual, removendo todos os caracteres que não são númericos.
+    let value = amount.value.replace(/\D/g, "")
+
+// Converte o valor para um número decimal, dividindo por 100.
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+// Formata o valor como moeda brasileira (BRL).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+    return value
+}
